116b0491258cc0f560f127d07c3a66c556e64676
danila_27ru <gorelko.danila@gmail.com>
2020-06-25 15:15:17 +0300

    removed redundant function

--- web_driver.py
+++ web_driver.py
@@ -1,14 +1,6 @@
 from selenium import webdriver
-import os
-import subprocess
-
 from location_manager import LocationManager
 
-
-def find(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
 
 class WebDriver:
     location = LocationManager()
